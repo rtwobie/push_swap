cb0289a2332cff27f7f3002f5c1d37342f57ee87
R2B <robeats.hale@gmail.com>
2025-02-22 20:02:34 +0100

    feat: separated testcases to .tester.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:35:55 by rha-le            #+#    #+#             */
-/*   Updated: 2025/02/20 21:15:36 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/02/22 17:15:52 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,14 @@
 #include "libft/libft.h"
 #include <stddef.h>
 
-void	print_arr(struct s_stack stack, int length, char c)
-{
-	int	i;
-
-	i = 0;
-	while (i < length)
-	{
-		ft_printf("%c: [num=%i, index=%i]\t", c, stack.entry[i].num, stack.entry[i].index);
-		i++;
-	}
-	ft_printf("\nsize: %i\n\n", stack.size);
-}
+// WARNING: remove when done!
+void	print_arr(t_stack a, t_stack b, int length);
+void	OP_TESTER(t_stack stack_a, t_stack stack_b, int argn);
 
 int	main(int argc, char *argv[])
 {
-	struct s_stack	stack_a;
-	struct s_stack	stack_b;
+	t_stack	stack_a;
+	t_stack	stack_b;
 	int				argn;
 
 	argn = create_lists(argc, argv, &stack_a.entry, &stack_b.entry);
@@ -39,12 +30,16 @@ int	main(int argc, char *argv[])
 		ft_printf("creating lists failed\n");
 		return (1);
 	}
+	stack_a.name = 'a';
 	stack_a.size = argn;
+	stack_b.name = 'b';
 	stack_b.size = 0;
-
 	sort(stack_a, stack_b);
-	print_arr(stack_a, argn, 'a');
+
+	OP_TESTER(stack_a, stack_b, argn);
 
 	free(stack_a.entry);
 	free(stack_b.entry);
 }
+
+// WARNING: Remember to remove .tester.c out of Makefile!
