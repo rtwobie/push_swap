b3e99f8f2efad91af307975c046fc1e18175dde0
Robbie Nhat Anh Ha Le <rha-le@c3a1c2.42berlin.de>
2025-02-24 17:03:49 +0100

    fix: op_manager

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 // WARNING: remove when done!
 void	print_arr(t_stack a, t_stack b, int length);
 void	OP_TESTER(t_stack stack_a, t_stack stack_b, int argn);
+int calc_pos_b(int idx_a, t_stack b);
 
 int	main(int argc, char *argv[])
 {
@@ -34,9 +35,10 @@ int	main(int argc, char *argv[])
 	stack_a.size = argn;
 	stack_b.name = 'b';
 	stack_b.size = 0;
+	op_manager(PUSH, 5, &stack_a, &stack_b);
 	sort(stack_a, stack_b);
-
-	OP_TESTER(stack_a, stack_b, argn);
+	print_arr(stack_a, stack_b, argn);
+	/*ft_printf("pos_b: %i", calc_pos_b(stack_a.entry[0].idx, stack_b));*/
 
 	free(stack_a.entry);
 	free(stack_b.entry);
