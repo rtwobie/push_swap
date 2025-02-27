5b4b04bcd9d3e29f7d1d61b90513b71b35be45c3
Robbie Nhat Anh Ha Le <rha-le@c3a9c4.42berlin.de>
2025-02-27 20:04:31 +0100

    feat: test script

---
+++ optest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check the first argument passed to the script and run the corresponding command
+case $1 in
+    1)
+        ./push_swap	1 3 5 -2 -1 2 6
+        ;;
+    2)
+        ./push_swap	5 1 3 -2 -1 2 6
+        ;;
+    3)
+        ./push_swap	3 5 1 -2 -1 2 6
+        ;;
+    *)
+		echo "Invalid Option"
+        ;;
+esac
+
+sed -n "${1}p" optestsolution
