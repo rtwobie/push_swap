13825fc487230d94d0e2a7d05a53bc105b016f9f
Robbie Nhat Anh Ha Le <rha-le@c3a6c6.42berlin.de>
2025-02-28 21:25:23 +0100

    refactor: passing pointer instead of whole struct

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 14:56:04 by rha-le            #+#    #+#             */
-/*   Updated: 2025/02/27 17:29:39 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/02/28 20:29:40 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ int	indexing(t_elem *a, int size)
 		x = 0;
 		while (j < size)
 		{
-			if (a[i].num == a[j].num && i != j)
+			if (a[i].val == a[j].val && i != j)
 				return (1);
-			if (a[i].num > a[j].num)
+			if (a[i].val > a[j].val)
 				x++;
 			j++;
 		}
@@ -40,32 +40,12 @@ int	indexing(t_elem *a, int size)
 	return (0);
 }
 
-// rotates entry position to beginning
-/*int	calc_ops(t_stack a, t_stack b, int entry_position, t_op *direction)*/
-/*{*/
-/*	int	pivot;*/
-/*	int	operations;*/
-/**/
-/*	pivot = stack.size / 2;*/
-/*	if ((stack.size - entry_position) >= pivot)*/
-/*	{*/
-/*		operations = entry_position - 1;*/
-/*		*direction = ROTATE;*/
-/*	}*/
-/*	else if ((stack.size - entry_position) < pivot)*/
-/*	{*/
-/*		operations = stack.size - entry_position + 1;*/
-/*		*direction = R_ROTATE;*/
-/*	}*/
-/*	return (operations);*/
-/*}*/
-
-void	sort(t_stack a, t_stack b)
+void	sort(t_stack *a, t_stack *b)
 {
-	if (indexing(a.entry, a.size) == 1)
+	if (indexing(a->entry, a->size) == 1)
 	{
-		free(a.entry);
-		free(b.entry);
+		free(a->entry);
+		free(b->entry);
 		exit(1);
 	}
 
