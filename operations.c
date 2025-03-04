b8d20c3752138802dfe7b542952a3b154d780d45
Robbie Nhat Anh Ha Le <rha-le@c3a9c2.42berlin.de>
2025-03-04 21:20:04 +0100

    refactor: changing the parameters to take in the pointer of struct

--- operations.c
+++ operations.c
@@ -6,47 +6,49 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 15:56:05 by rha-le            #+#    #+#             */
-/*   Updated: 2025/02/27 18:24:52 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/03/04 20:52:55 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
 
-void	swap(t_stack stack)
+void	swap(t_stack *stack)
 {
 	t_elem	temp;
 
-	temp = stack.entry[1];
-	stack.entry[1] = stack.entry[0];
-	stack.entry[0] = temp;
+	temp = stack->entry[1];
+	stack->entry[1] = stack->entry[0];
+	stack->entry[0] = temp;
 }
 
-void	rrot(t_stack stack)
+void	rrot(t_stack *stack)
 {
+	int		i;
 	t_elem	temp;
 
-	temp = stack.entry[stack.size - 1];
-	while (--stack.size > 0)
+	i = stack->size;
+	temp = stack->entry[stack->size - 1];
+	while (--i > 0)
 	{
-		stack.entry[stack.size] = stack.entry[stack.size - 1];
+		stack->entry[i] = stack->entry[i - 1];
 	}
-	stack.entry[0] = temp;
+	stack->entry[0] = temp;
 }
 
-void	rot(t_stack stack)
+void	rot(t_stack *stack)
 {
 	int		i;
 	t_elem	temp;
 
 	i = 0;
-	temp = stack.entry[0];
-	while (i < stack.size)
+	temp = stack->entry[0];
+	while (i < stack->size)
 	{
-		stack.entry[i] = stack.entry[i + 1];
+		stack->entry[i] = stack->entry[i + 1];
 		i++;
 	}
-	stack.entry[stack.size - 1] = temp;
+	stack->entry[stack->size - 1] = temp;
 }
 
 void	push(t_stack *src, t_stack *dest)
