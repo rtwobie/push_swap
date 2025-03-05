a95398a20c47c2fdd10e7f68ba19bec35d3a4025
R2B <robeats.hale@gmail.com>
2025-03-05 02:19:46 +0100

    refactor(not tested): easier to use subfunctions of op_manager()

--- operation_manager.c
+++ operation_manager.c
@@ -6,77 +6,82 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 16:17:06 by rha-le            #+#    #+#             */
-/*   Updated: 2025/03/04 20:52:05 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/03/05 01:59:41 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
 
-void	operation(t_stack *stack, t_op type)
+void	operation(t_op type, int amount, t_stack *stack)
 {
-	if (type == SWAP)
+	while (amount > 0)
 	{
-		swap(stack);
-		ft_printf("s%c\n", stack->name);
-	}
-	if (type == ROTATE)
-	{
-		rot(stack);
-		ft_printf("r%c\n", stack->name);
-	}
-	if (type == R_ROTATE)
-	{
-		rrot(stack);
-		ft_printf("rr%c\n", stack->name);
+		if (type == SWAP)
+		{
+			swap(stack);
+			ft_printf("s%c\n", stack->name);
+		}
+		if (type == ROTATE)
+		{
+			rot(stack);
+			ft_printf("r%c\n", stack->name);
+		}
+		if (type == R_ROTATE)
+		{
+			rrot(stack);
+			ft_printf("rr%c\n", stack->name);
+		}
+		amount--;
 	}
 }
 
-void	operation2(t_stack *stack1, t_stack *stack2, t_op type)
+void	operation2(t_op type, int amount, t_stack *stack1, t_stack *stack2)
 {
-	if (type == ROTATE2)
+	while (amount > 0)
 	{
-		rot(stack1);
-		rot(stack2);
-		ft_printf("rr\n");
-	}
-	if (type == R_ROTATE2)
-	{
-		rrot(stack1);
-		rrot(stack2);
-		ft_printf("rrr\n");
-	}
-	if (type == SWAP2)
-	{
-		swap(stack1);
-		swap(stack2);
-		ft_printf("ss\n");
-	}
-	if (type == PUSH)
-	{
-		push(stack1, stack2);
-		ft_printf("p%c\n", stack2->name);
+		if (type == ROTATE2)
+		{
+			rot(stack1);
+			rot(stack2);
+			ft_printf("rr\n");
+		}
+		if (type == R_ROTATE2)
+		{
+			rrot(stack1);
+			rrot(stack2);
+			ft_printf("rrr\n");
+		}
+		if (type == SWAP2)
+		{
+			swap(stack1);
+			swap(stack2);
+			ft_printf("ss\n");
+		}
+		amount--;
 	}
 }
 
 void	op_manager(t_op type, int amount, t_stack *stack1, t_stack *stack2)
 {
-	while (amount > 0)
+	if (type == SWAP)
+		operation(SWAP, amount, stack1);
+	if (type == ROTATE)
+		operation(ROTATE, amount, stack1);
+	if (type == R_ROTATE)
+		operation(R_ROTATE, amount, stack1);
+	if (type == SWAP2)
+		operation2(SWAP2, amount, stack1, stack2);
+	if (type == ROTATE2)
+		operation2(SWAP2, amount, stack1, stack2);
+	if (type == R_ROTATE2)
+		operation2(SWAP2, amount, stack1, stack2);
+	if (type == PUSH)
 	{
-		if (type == SWAP)
-			operation(stack1, SWAP);
-		if (type == ROTATE)
-			operation(stack1, ROTATE);
-		if (type == R_ROTATE)
-			operation(stack1, R_ROTATE);
-		if (type == SWAP2)
-			operation2(stack1, stack2, SWAP2);
-		if (type == ROTATE2)
-			operation2(stack1, stack2, ROTATE2);
-		if (type == R_ROTATE2)
-			operation2(stack1, stack2, R_ROTATE2);
-		if (type == PUSH)
-			operation2(stack1, stack2, PUSH);
-		amount--;
+		while (amount-- > 0)
+		{
+			push(stack1, stack2);
+			ft_printf("p%c\n", stack2->name);
+		}
 	}
 }
