32b972a04561467ef6ea3d3cc1e0209729e941ae
R2B <robeats.hale@gmail.com>
2025-03-06 02:50:44 +0100

    refactor: only entails main() and necessary #includes

--- src/main.c
+++ src/main.c
@@ -15,62 +15,6 @@
 #include "defs.h"
 #include "input.h"
 #include "sort.h"
-#include "sort_utils.h"
-#include "operations.h"
-
-int	create_lists(int argc, char *argv[], t_elem **stack_a, t_elem **stack_b)
-{
-	char	**args;
-	int		argn;
-
-	if (argc == 1)
-		return (0);
-	args = argv + 1;
-	argn = argc - 1;
-	if (!is_argvalid(args))
-		return (0);
-	if (argc == 2)
-	{
-		args = ft_split(argv[1], ' ');
-		argn = count_args(args);
-	}
-	*stack_a = atoi_arr(argn, args);
-	*stack_b = (t_elem *) ft_calloc(sizeof(t_elem), argn);
-	if (!*stack_b)
-		return (0);
-	if (argc == 2)
-		free_args(args);
-	return (argn);
-}
-
-int	sort(t_stack *a, t_stack *b)
-{
-	int	i;
-
-	if (indexing(a) != 0)
-		return (1);
-	if (is_sorted(a) || a->size < 3)
-	{
-		rot_to_top(a, find_smallest(a));
-		return (0);
-	}
-	if (a->size == 4)
-		op_manager(PUSH, 1, a, b);
-	else if (a->size >= 5)
-		op_manager(PUSH, 2, a, b);
-	i = a->size;
-	while (i > 3)
-	{
-		move_manager(a, b);
-		op_manager(PUSH, 1, a, b);
-		i--;
-	}
-	sort_three(a);
-	rot_to_top(b, find_biggest(b));
-	push_back(a, b);
-	rot_to_top(a, find_smallest(a));
-	return (0);
-}
 
 int	main(int argc, char *argv[])
 {
@@ -78,15 +22,17 @@ int	main(int argc, char *argv[])
 	t_stack	stack_b;
 	int		argn;
 
-	argn = create_lists(argc, argv, &stack_a.entry, &stack_b.entry);
+	argn = create_lists(argc, argv, &stack_a, &stack_b);
 	if (!argn || !stack_a.entry)
 	{
 		ft_printf("Error\n");
 		return (1);
 	}
+	if (argn == 1)
+		return (0);
 	stack_a.name = 'a';
-	stack_a.size = argn;
 	stack_b.name = 'b';
+	stack_a.size = argn;
 	stack_b.size = 0;
 	if (sort(&stack_a, &stack_b) != 0)
 	{
